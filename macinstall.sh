44adc88be8cf5fd6e30ae6e09d399a7592dd7588
Justin Kanwischer <56719370+jstn-ck@users.noreply.github.com>
2021-03-07 19:08:45 +0100

    Update macinstall.sh

--- macinstall.sh
+++ macinstall.sh
@@ -3,7 +3,6 @@
 # xcode for brew
 xcode-select --install
 
-
 # Install brew package manager
 echo "Installing brew.."
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
@@ -13,14 +12,13 @@ echo "Installing Dependencies.."
 brew install stow wget curl cmake zsh zsh-syntax-highlighting fzf neovim
 
 #install ohmyzsh
+echo "Installing OhMyZsh.."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Zsh to default shell"
 chsh -s $(which zsh)
-stow zsh
+
 # Vim plug / Plugin manager
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-stow nvim
 
 nvim +PlugInstall +qall

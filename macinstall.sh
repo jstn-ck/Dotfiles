da95c90d515f38dc3e1aaa07361ea59c952b1767
Justin Kanwischer <56719370+jstn-ck@users.noreply.github.com>
2021-03-07 19:30:38 +0100

    Update macinstall.sh

--- macinstall.sh
+++ macinstall.sh
@@ -20,6 +20,9 @@ chsh -s $(which zsh)
 
 # Make dirs
 mkdir ~/nvim
+#Move Files from dotfiles
+mv .vimrc ~/.vimrc
+mv .zshrc ~/.zshrc
 
 # Vim plug / Plugin manager
 curl -fLo ~/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

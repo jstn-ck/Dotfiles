cf48d0d84b9becb6f4244d350cec893050beb566
Justin Kanwischer <kanwischer.justinc@gmail.com>
2021-07-10 23:40:33 +0200

    added config files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,18 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias ls "ls -la"
+alias gs "git status"
+alias vimrc "vim ~/.vimrc"
+alias i3c "vim ~/.config/i3/config"
+
+set -gx EDITOR vim
+

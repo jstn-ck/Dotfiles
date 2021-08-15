69771db567922a1eb825add3a358479d8b3a00d7
Justin Kanwischer <56719370+jstn-ck@users.noreply.github.com>
2021-08-15 15:34:38 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -12,9 +12,18 @@ set -g theme_hostname always
 # aliases
 alias ls "ls -la"
 alias gs "git status"
-alias vimrc "vim ~/.vimrc"
+alias vim "nvim"
+alias vimrc "vim ~/.config/nvim/init.vim"
+alias work "cd ~/Documents/work/f7justin/projects"
+alias config "cd ~/.config/"
+alias lint "ddev fe-lintfix"
 alias fix "npm run stylelint-fix && npm run eslint-fix"
-alias fishconf "vim ~/.config/fish/config.fish"
-alias work "cd ~/Documents/F7"
+alias npmcopy "rm -rf ./dist/Dummy ./dist/Fonts ./dist/Images && cp -r ./src/Images ./dist/ && cp -r ./src/Fonts ./dist/ && cp -r ./src/Dummy ./dist/ && echo 'done'"
+alias fishconf "~/.config/fish/config.fish"
+alias ide="tmux split-window -v -p 30; tmux split-window -h -p 66; tmux split-window -h -p 50"
 
 set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH

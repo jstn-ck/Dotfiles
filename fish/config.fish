0d42f1acfa0cfc28b533ad1406d0991306a5cea8
Justin Kanwischer <56719370+jstn-ck@users.noreply.github.com>
2021-10-04 16:42:56 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -19,9 +19,9 @@ alias config "cd ~/.config/"
 alias lint "ddev fe-lintfix"
 alias fix "npm run stylelint-fix && npm run eslint-fix"
 alias npmcopy "rm -rf ./dist/Dummy ./dist/Fonts ./dist/Images && cp -r ./src/Images ./dist/ && cp -r ./src/Fonts ./dist/ && cp -r ./src/Dummy ./dist/ && echo 'done'"
-alias fishconf "~/.config/fish/config.fish"
+alias fishconf "nvim ~/.config/fish/config.fish"
 alias ide="tmux split-window -v -p 30; tmux split-window -h -p 66; tmux split-window -h -p 50"
-alias brewup=‘brew update; brew upgrade; brew cleanup; brew doctor’
+alias brewup="brew update; brew upgrade; brew cleanup; brew doctor"
 
 set -gx EDITOR nvim
 

877ab02adb4b53c1c5469b20d85bfe233e1f1177
Justin Kanwischer <56719370+jstn-ck@users.noreply.github.com>
2022-04-20 17:37:58 +0200

    Add files via upload

---
+++ nvim-latest/init.lua
@@ -0,0 +1,83 @@
+require('packer_init')
+require('autocmds')
+require('maps')
+require('plugins/indent-blankline')
+require('plugins/nvim-cmp')
+require('plugins/nvim-lspconfig')
+require('plugins/nvim-treesitter')
+
+local g = vim.a
+local opt = vim.opt
+
+opt.mouse = 'a'
+opt.ve = 'onemore'
+opt.clipboard = 'unnamedplus'
+opt.swapfile = false
+opt.completeopt = 'menuone,noselect'
+opt.number = true
+opt.showmatch = true
+opt.foldmethod = 'marker'
+opt.colorcolumn = '80'
+opt.relativenumber = true
+opt.splitright = true
+opt.splitbelow = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.linebreak = true
+opt.termguicolors = true
+opt.laststatus= 2
+opt.title = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.smartindent = true
+opt.autoindent = true
+opt.showcmd = true
+opt.cmdheight = 1
+opt.hidden = true
+opt.history = 100
+opt.lazyredraw = true
+opt.synmaxcol = 240
+opt.updatetime = 700
+opt.shell = 'fish'
+opt.shortmess:append "sI"
+opt.hlsearch = true
+opt.smarttab = true
+opt.path = '**'
+opt.wildignore = '*/node-modules/*'
+opt.cursorline = true
+
+opt.undodir = '~/.config/nvim/.undo//'
+
+-- TODO DISABLE BUILTINS
+-- local disabled_built_ins = {
+--   "netrw",
+--   "netrwPlugin",
+--   "netrwSettings",
+--   "netrwFileHandlers",
+--   "gzip",
+--   "zip",
+--   "zipPlugin",
+--   "tar",
+--   "tarPlugin",
+--   "getscript",
+--   "getscriptPlugin",
+--   "vimball",
+--   "vimballPlugin",
+--   "2html_plugin",
+--   "logipat",
+--   "rrhelper",
+--   "spellfile_plugin",
+--   "matchit"
+-- }
+
+vim.cmd[[
+  highlight Visual cterm=NONE ctermbg=236 ctermfg=NONE guibg=Grey40
+  highlight LineNr cterm=none ctermfg=240 guifg=#2b506e guibg=#000000
+  augroup BgHighlight
+    autocmd!
+    autocmd WinEnter * set cul
+    autocmd WinLeave * set nocul
+  augroup END
+  colorscheme NeoSolarized
+]]
---
+++ nvim-latest/lua/packer_init.lua
@@ -0,0 +1,70 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+end
+
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+  return
+end
+
+-- JCK Plugins
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup()
+    end
+  }
+  use 'kyazdani42/nvim-web-devicons'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'neovim/nvim-lspconfig'
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'saadparwaiz1/cmp_luasnip',
+    },
+  }
+  use 'hoob3rt/lualine.nvim'
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+  use 'kristijanhusak/defx-git'
+  use 'kristijanhusak/defx-icons'
+  use 'Shougo/defx.nvim'
+  use 'editorconfig/editorconfig-vim'
+  use 'mg979/vim-visual-multi'
+  use 'nvim-telescope/telescope-fzf-native.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-lua/popup.nvim'
+  use 'folke/lsp-colors.nvim'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
+  use 'mattn/emmet-vim'
+  use 'tpope/vim-commentary'
+
+
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)

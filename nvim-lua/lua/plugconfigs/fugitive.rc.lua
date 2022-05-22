e268fb6fd1572d7d17e63139a478ed7d3a2e28ea
Justin Kanwischer <56719370+jstn-ck@users.noreply.github.com>
2022-05-22 18:23:40 +0200

    Delete nvim-lua directory

--- nvim-lua/lua/plugconfigs/fugitive.rc.lua
+++
@@ -1,5 +0,0 @@
-if vim.fn.exists('*fugitive#statusline') == 0 then
-  vim.opt.statusline=%F\ %m%r%h%w%y%{'['.(&fenc!=''?&fenc:&enc).':'.&ff.']'}[L%l/%L,C%03v]
-  vim.opt.statusline+=%=
-  vim.opt.statusline+=%{fugitive#statusline()}
-end
